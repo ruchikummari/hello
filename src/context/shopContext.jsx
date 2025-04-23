950f94d2fdc1c57564704b0b58eee930c42f2575
Ruchi kumari <ruchikumari@Ruchis-MacBook-Air.local>
2025-04-23 23:10:45 +0530

    Saved all file changes

--- src/context/shopContext.jsx
+++ src/context/shopContext.jsx
@@ -1,18 +1,20 @@
 import React, { createContext } from 'react'
-export const shopContext = createContext();
-const ShopContextProvider=(props)=>{
+export const ShopContext = createContext();
 
+const ShopContextProvider=(props)=>{
     const currency = '$';
     const delivery_fee=10;
 
     const value={
-        currency , delivery_fee
+        currency,
+        delivery_fee
     }
 
-  return (
-    <ShopContextProvider value={value}>
-        {props.children}
-    </ShopContextProvider>
-  )
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
 }
+
 export default ShopContextProvider
