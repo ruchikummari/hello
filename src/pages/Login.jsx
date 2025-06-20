3dcc40dee2f3acf808f3cd1371976a13620270b1
Ruchi kumari <ruchikumari@Ruchis-MacBook-Air.local>
2025-06-20 11:24:02 +0530

    Saved all file changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-
+import Title from '../components/Title';
 function Login() {
-    const[currentState,setCurrentState]=useState('Sign Up');
+    // const[currentState,setCurrentState]=useState('Sign Up');
   return (
     <form className='flex flex-col items-center w-90% sm:max-w-96 m-auto mt-14 gap-4 text-gray-700'>
-        <div className='inline flex items-center gap-2 mt-10 mb-2'>
-            <p className='prata-regular text-3xl'>{currentState}</p>
-            <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
+        <div className='text-2xl inline flex items-center gap-2 mt-10 mb-2'>
+            <Title text1={'SIGN'} text2={' UP'}/>
+            {/* <p className='prata-regular text-3xl'>{currentState}</p>
+            <hr className='border-none h-[1.5px] w-8 bg-gray-800'/> */}
         </div>
         <input type='text' className='px-3 py-2 border rounded border-gray-800' placeholder='Name' required/>
         <input type='email' className='px-3 py-2 border rounded border-gray-800' placeholder='Email'required/>
