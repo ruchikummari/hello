ade9d9eceddd924ff22a973bc1f41096dfb67c10
Ruchi kumari <ruchikumari@Ruchis-MacBook-Air.local>
2025-07-22 19:39:06 +0530

    Saved all file changes

---
+++ backend/middleware/adminAuth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+const adminAuth = async (req, res, next) => {
+    try {
+        const { token } = req.headers;
+
+        if (!token) {
+            return res.json({ success: false, message: "Not Authorized. Login Again." });
+        }
+
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+        if(token_decodev !==process.env.ADMIN_EMAIL+process.env.ADMIN_PASSWORD){
+            return res.json({ success: false, message: "Not Authorized. Login Again." });
+        }
+
+        // Optional: Attach decoded token to request if needed
+        // req.admin = token_decode;
+
+        next();  // Proceed to the next middleware or route
+    } catch (error) {
+        return res.json({ success: false, message: error.message});
+    }
+};
+
+export default adminAuth;
